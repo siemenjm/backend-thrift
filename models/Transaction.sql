cf7106fb942ce831bae2b856ecb51df4200bb158
Jared Siemen <siemenjm@gmail.com>
2022-10-09 20:17:21 -0400

    Add Transaction model and seed data

---
+++ models/Transaction.sql
@@ -0,0 +1,15 @@
+CREATE TABLE transactions (
+    trans_id SERIAL PRIMARY KEY,
+    date DATE NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    amount DECIMAL (11, 2) NOT NULL,
+    trans_type VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    sub_category VARCHAR(255),
+    credited_account_id INT,
+    debited_account_id INT,
+    user_id INT NOT NULL,
+    FOREIGN KEY (credited_account_id) REFERENCES accounts (account_id),
+    FOREIGN KEY (debited_account_id) REFERENCES accounts (account_id),
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
