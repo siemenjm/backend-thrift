2bc56c599bb7aa48419bbc179d5b289e203250eb
Jared Siemen <siemenjm@gmail.com>
2022-10-13 10:45:02 -0400

    Fix institution balance update when deleting an account

--- controllers/accounts_controller.js
+++ controllers/accounts_controller.js
@@ -64,10 +64,13 @@ router.put('/:accountId', async (req, res) => {
 router.delete('/:accountId', async (req, res) => {
     try {
         const { accountId } = req.params;
+        const institutionID = await pool.query('SELECT ins_id FROM accounts WHERE account_id = $1', [accountId]);
+
         const deleteAccount = await pool.query('DELETE FROM accounts WHERE account_id = $1', [accountId]);
 
         // Update Institution balance after deleting account
-        updateInstitutionBalance(accountId);
+        const institutionBalance = await pool.query('SELECT SUM(current_balance) FROM accounts WHERE ins_id = $1', [institutionID.rows[0].ins_id]);
+        const updateInstitution = await pool.query('UPDATE institutions SET current_balance = $1 WHERE ins_id = $2', [institutionBalance.rows[0].sum, institutionID.rows[0].ins_id]);
 
         res.json(`Account with account_id = ${accountId} was deleted`);
     } catch (error) {
